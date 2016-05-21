6c1774815ca64965502050afcf9d590945c1ddfd
Ben Copeland <bencopeland@Bens-MacBook-Pro.local>
2016-05-21 11:09:22 -0500

    new item input functionality

--- app/app.js
+++ app/app.js
@@ -6,7 +6,41 @@ app.controller("NavCtrl", function($scope){
 
 app.controller("TodoCtrl", function($scope){
 	$scope.welcome = "hello";
-	$scope.showListView = true;
+	$scope.showListView = false;
+	$scope.newTask = "";
+
+	$scope.items = [
+		{
+			id: 0,
+			task: "mow the lawn",
+			isCompleted: true,
+			dueDate: "12/5/17",
+			assignedTo: "greg",
+			location: "Zoes House",
+			urgency: "low",
+			dependencies: "sunshine, clippers, hat, water, headphones"
+		},
+				{
+			id: 1,
+			task: "grade quizzes",
+			isCompleted: false,
+			dueDate: "12/5/17",
+			assignedTo: "joe",
+			location: "NSS",
+			urgency: "high",
+			dependencies: "WIFI, tissues, vodka"
+		},
+				{
+			id: 2,
+			task: "take a nap",
+			isCompleted: false,
+			dueDate: "5/21/16",
+			assignedTo: "Zoe",
+			location: "Zoes House",
+			urgency: "medium",
+			dependencies: "cat, hammock, pillow"
+		}
+	];
 
 	$scope.newItem = function(){
 		console.log("new item");
@@ -16,5 +50,28 @@ app.controller("TodoCtrl", function($scope){
 		console.log("all item");
 		$scope.showListView = true;
 	};
+	$scope.addNewItem = function(){
+		$scope.newTask.isCompleted = false;
+		$scope.newTask.id = $scope.items.length;
+		console.log("new item: ", $scope.newTask);
+		$scope.items.push($scope.newTask);
+		$scope.newTask = "";
+	};
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
