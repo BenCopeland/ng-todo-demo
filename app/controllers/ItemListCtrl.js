92ab606364e23a9a4a70eb012e5e1701d4efbc4d
Ben Copeland <bencopeland@Bens-MacBook-Pro.local>
2016-05-23 18:25:37 -0500

    added data directory

--- app/controllers/ItemListCtrl.js
+++ app/controllers/ItemListCtrl.js
@@ -1,34 +1,3 @@
 app.controller("ItemListCtrl", function($scope){
-	$scope.items = [
-		{
-			id: 0,
-			task: "mow the lawn",
-			isCompleted: true,
-			dueDate: "12/5/17",
-			assignedTo: "greg",
-			location: "Zoes House",
-			urgency: "low",
-			dependencies: "sunshine, clippers, hat, water, headphones"
-		},
-				{
-			id: 1,
-			task: "grade quizzes",
-			isCompleted: false,
-			dueDate: "12/5/17",
-			assignedTo: "joe",
-			location: "NSS",
-			urgency: "high",
-			dependencies: "WIFI, tissues, vodka"
-		},
-				{
-			id: 2,
-			task: "take a nap",
-			isCompleted: false,
-			dueDate: "5/21/16",
-			assignedTo: "Zoe",
-			location: "Zoes House",
-			urgency: "medium",
-			dependencies: "cat, hammock, pillow"
-		}
-	];
+	$scope.items = [];
 });
--- app/controllers/ItemNewCtrl.js
+++ app/controllers/ItemNewCtrl.js
@@ -1,37 +1,6 @@
 app.controller("ItemNewCtrl", function($scope){
 	$scope.newTask = {};
-	$scope.items = [
-		{
-			id: 0,
-			task: "mow the lawn",
-			isCompleted: true,
-			dueDate: "12/5/17",
-			assignedTo: "greg",
-			location: "Zoes House",
-			urgency: "low",
-			dependencies: "sunshine, clippers, hat, water, headphones"
-		},
-				{
-			id: 1,
-			task: "grade quizzes",
-			isCompleted: false,
-			dueDate: "12/5/17",
-			assignedTo: "joe",
-			location: "NSS",
-			urgency: "high",
-			dependencies: "WIFI, tissues, vodka"
-		},
-				{
-			id: 2,
-			task: "take a nap",
-			isCompleted: false,
-			dueDate: "5/21/16",
-			assignedTo: "Zoe",
-			location: "Zoes House",
-			urgency: "medium",
-			dependencies: "cat, hammock, pillow"
-		}
-	];
+	$scope.items = [];
 
 	$scope.addNewItem = function(){
 		$scope.newTask.isCompleted = false;
