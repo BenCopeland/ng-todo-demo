3539d5ff4d07b86fdfb8a616b60966eb5eef7949
Ben Copeland <bencopeland@Bens-MacBook-Pro.local>
2016-05-23 20:57:28 -0500

    buncha stuff

--- app/controllers/ItemListCtrl.js
+++ app/controllers/ItemListCtrl.js
@@ -1,14 +1,26 @@
 app.controller("ItemListCtrl", function($scope, $http){
 	$scope.items = [];
-
-	$http.get("https://todo-app6.firebaseio.com/items.json")
-	.success(function(itemObject){
-		var itemCollection = itemObject;
-		console.log(itemCollection);
-		Object.keys(itemCollection).forEach(function(key){
-			itemCollection[key].id = key;
-			$scope.items.push(itemCollection[key]);
+	var getItems = function(){
+		$http.get("https://todo-app6.firebaseio.com/items.json")
+		.success(function(itemObject){
+			var itemCollection = itemObject;
+			// console.log(itemCollection);
+			Object.keys(itemCollection).forEach(function(key){
+				itemCollection[key].id = key;
+				$scope.items.push(itemCollection[key]);
+			});
 		});
-	});
+	};
+	getItems();
 
+	$scope.itemDelete = function(itemId){
+		console.log(itemId);
+		$http
+			.delete(`https://todo-app6.firebaseio.com/items/${itemId}.json`)
+			.success(function(response){
+			// console.log(response);
+			$scope.items = [];
+			getItems();
+		});
+	};
 });
