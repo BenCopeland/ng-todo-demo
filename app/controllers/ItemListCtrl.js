119849a665b8a04732d9ac11c71b81006d9a2d6c
Ben Copeland <bencopeland@Bens-MacBook-Pro.local>
2016-05-23 19:19:10 -0500

    firebase data storing

--- app/controllers/ItemListCtrl.js
+++ app/controllers/ItemListCtrl.js
@@ -1,3 +1,14 @@
-app.controller("ItemListCtrl", function($scope){
+app.controller("ItemListCtrl", function($scope, $http){
 	$scope.items = [];
+
+	$http.get("https://todo-app6.firebaseio.com/items.json")
+	.success(function(itemObject){
+		var itemCollection = itemObject;
+		console.log(itemCollection);
+		Object.keys(itemCollection).forEach(function(key){
+			itemCollection[key].id = key;
+			$scope.items.push(itemCollection[key]);
+		});
+	});
+
 });
