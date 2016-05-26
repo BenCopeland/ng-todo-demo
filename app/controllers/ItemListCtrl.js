5f138491b18386c0748b976092ffa6bf6a98a301
Ben Copeland <bencopeland@Bens-MacBook-Pro.local>
2016-05-25 21:14:03 -0500

    killed console logs

--- app/controllers/ItemListCtrl.js
+++ app/controllers/ItemListCtrl.js
@@ -2,12 +2,12 @@ app.controller("ItemListCtrl", function($scope, itemStorage){
 	$scope.items = [];
 
 	itemStorage.getItemList().then(function(itemCollection){
-		console.log(itemCollection);
+		// console.log(itemCollection);
 		$scope.items = itemCollection;
 	});
 
 	$scope.itemDelete = function(itemId){
-        console.log("itemId", itemId);
+        // console.log("itemId", itemId);
         itemStorage.deleteItem(itemId).then(function(response){
             itemStorage.getItemList().then(function(itemCollection){
                 $scope.items = itemCollection;
--- app/controllers/ItemViewCtrl.js
+++ app/controllers/ItemViewCtrl.js
@@ -2,10 +2,10 @@
 app.controller("ItemViewCtrl", function($scope, $routeParams, itemStorage){
     $scope.items = [];
     $scope.selectedItem = {};
-    console.log($routeParams.itemId);
+    // console.log($routeParams.itemId);
 
     itemStorage.getItemList().then(function(itemCollection){
-        console.log("itemCollection from promise", itemCollection);
+        // console.log("itemCollection from promise", itemCollection);
         $scope.items = itemCollection;
 
         $scope.selectedItem = $scope.items.filter(function(item){
